74c8e65636e7584a87f786b35d258a862951cb14
Thien Nguyen <t.nguyen@outlook.com>
2016-04-09 20:12:12 +0100

    fixed install.sh directory issue thing

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@
 
 ########## Variables
 
-dir=~/Documents/dotfiles                    # dotfiles directory
+dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
 files="bashrc vimrc vim zshrc oh-my-zsh"    # list of files/folders to symlink in homedir
 
