1da3334a66e85063d003a475fe2f37a990713836
@thien <thien@users.noreply.github.com>
2020-03-03 22:52:50 +0100

    update ents

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
 # dupes alacritty
-cat alacritty/alacritty.yml > ~/.alacritty.yml
+cat alacritty/alacritty.yml > $HOME/.config/alacritty/alacritty.yml
 
 # update tmux
-# cat tmux/tmux.conf > ~/.tmux.conf
+cat tmux/tmux.conf > ~/.tmux.conf
 
 # update vim
 cat vim/vimrc > ~/.vimrc
