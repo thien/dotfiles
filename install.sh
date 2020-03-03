edeb52e52068ebca02edcc801cb632a1d8dc266e
Thien Nguyen <t.n@apple.com>
2020-03-03 22:43:45 +0100

    add readme and installers

---
+++ install.sh
@@ -0,0 +1,10 @@
+
+brew install watch
+brew install nethogs
+
+# get cpu temp info
+git clone https://github.com/lavoiesl/osx-cpu-temp.git
+cd osx-cpu-temp
+sudo make install
+cd ..
+
---
+++ setup.sh
@@ -0,0 +1,8 @@
+# dupes alacritty
+cat alacritty/alacritty.yml > ~/.alacritty.yml
+
+# update tmux
+# cat tmux/tmux.conf > ~/.tmux.conf
+
+# update vim
+cat vim/vimrc > ~/.vimrc
