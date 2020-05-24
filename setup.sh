9d39573bbd05cbd82b564da5c84e972c29fbc1bd
@thien <thien@users.noreply.github.com>
2020-05-24 19:56:53 +0200

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,4 +5,9 @@ cat alacritty/alacritty.yml > $HOME/.config/alacritty/alacritty.yml
 cat tmux/tmux.conf > ~/.tmux.conf
 
 # update vim
+if [[ "$OSTYPE" == "darwin"* ]]; then
+	# I guess I could check whether this is already installed
+	brew install vim
+fi
 cat vim/vimrc > ~/.vimrc
+
