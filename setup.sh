38b43bc15de5f370ee705463344c6363517d66a1
@thien <thien@users.noreply.github.com>
2020-05-24 20:01:31 +0200

    check if vundle is installed

--- setup.sh
+++ setup.sh
@@ -9,5 +9,11 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
 	# I guess I could check whether this is already installed
 	brew install vim
 fi
+# check if vundle is installed
+VUNDLE_DIR="~/.vim/bundle/Vundle.vim"
+if [ ! -d $VUNDLE_DIR ]; then
+	git clone https://github.com/VundleVim/Vundle.vim.git $VUNDLE_DIR
+fi
+# update vimrc
 cat vim/vimrc > ~/.vimrc
 
